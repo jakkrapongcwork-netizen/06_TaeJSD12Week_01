673d29154c9976bdde199e51236e24a43af39840
jakkrapongcwork-netizen <jakkrapongc.work@gmail.com>
2026-03-06 14:02:35 +0700

    added objectandarray files

---
+++ object-and-array.js
@@ -0,0 +1,5 @@
+let learner = {name:"Tae",age:27,isGraduated:"19 Jun 26",hobbies:["playgames", "music", "exercise"]}
+console.log("name: "+learner.name);
+console.log("age: "+learner.age);
+console.log("Graduated: "+learner.isGraduated);
+console.log("hobbies: "+learner.hobbies.slice(0,3));
