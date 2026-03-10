9494678706d69142587a28b907f37e9d734c3599
jakkrapongcwork-netizen <jakkrapongc.work@gmail.com>
2026-03-10 13:18:59 +0700

    added comment

--- object-and-array.js
+++ object-and-array.js
@@ -3,3 +3,11 @@ console.log("name: "+learner.name);
 console.log("age: "+learner.age);
 console.log("Graduated: "+learner.isGraduated);
 console.log("hobbies: "+learner.hobbies.slice(0,3));
+
+let name2 = "Tae";
+let age2 = 27;
+let isGraduated2 = "19 Jun 26";
+let hobbies2 = ["play game","music","exercise"];
+
+let learner2 = "Name : "+name2+", Age : "+age2 +", IsGraduated : "+isGraduated2 +", Hobbies : "+hobbies2.slice(0,3);
+console.log(learner2);
