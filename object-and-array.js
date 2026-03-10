06c6ebab0db818324d3759262faa0b5f0f7dbc68
jakkrapongcwork-netizen <jakkrapongc.work@gmail.com>
2026-03-10 19:05:52 +0700

    update object-and-array

--- object-and-array.js
+++ object-and-array.js
@@ -4,10 +4,10 @@ console.log("age: "+learner.age);
 console.log("Graduated: "+learner.isGraduated);
 console.log("hobbies: "+learner.hobbies.slice(0,3));
 
-let name2 = "Tae";
-let age2 = 27;
-let isGraduated2 = "19 Jun 26";
-let hobbies2 = ["play game","music","exercise"];
-
-let learner2 = "Name : "+name2+", Age : "+age2 +", IsGraduated : "+isGraduated2 +", Hobbies : "+hobbies2.slice(0,3);
-console.log(learner2);
+// ไม่แน่ใจว่าวิธีเขียน Object array โดยใช้ console.log อันเดียวแสดงเขียนยังไง
+// let name2 = "Tae";
+// let age2 = 27;
+// let isGraduated2 = "19 Jun 26";
+// let hobbies2 = ["play game","music","exercise"];
+// let learner2 = {"Name : "+name2+", Age : "+age2 +", IsGraduated : "+isGraduated2 +", Hobbies : "+hobbies2.slice(0,3);}
+// console.log(learner2);
